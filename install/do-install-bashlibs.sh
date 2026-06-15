253a0462536aec23b60c9a64237bd0caa7cd95df
Mac Harwood <MacHarwood@gmail.com>
2026-06-15 12:51:01 +1000

    chore: do-install-bashlibs.sh -> install/do-install-bashlibs.sh

--- install/do-install-bashlibs.sh
+++ install/do-install-bashlibs.sh
@@ -2,6 +2,7 @@
 
 ENV_ROOT=_none_
 SUGGEST_HOW_TO_INSTALL_TO_ROOT=no
+THIS_DIR_REL="../"
 
 function apps_doInstallOrClean()
 {
@@ -9,4 +10,4 @@ function apps_doInstallOrClean()
     do_exeInstall_orClean do-run-in-docker
 }
 
-source "$(dirname "${BASH_SOURCE[0]}")/lib-build-funcs.inc.bash"
+source "$(dirname "${BASH_SOURCE[0]}")/../lib-build-funcs.inc.bash"
