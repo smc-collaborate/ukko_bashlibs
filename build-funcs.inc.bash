f2fb34a9d6adda770f79b736ce0d3f8727ba5446
Mac Harwood <MacHarwood@gmail.com>
2026-06-29 14:06:00 +1000

    chore: Remove unused shim warnings

--- build-funcs.inc.bash
+++
@@ -1,8 +0,0 @@
-{
-    echo "❓  | Deprecated - use 'lib-building.inc.bash' instead of 'build-funcs.inc.bash'"
-    echo "❓  | Path:"
-    for x in "${BASH_SOURCE[@]}" ; do
-        echo "❓  |   - $x"
-    done
-} >&2
-source "$(dirname "$(realpath -m "${BASH_SOURCE[0]}")")/lib-building.inc.bash"
