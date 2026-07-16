383b9796255628b61f355f3c6c9e9bada6fa4170
Mac Harwood <MacHarwood@gmail.com>
2026-07-16 11:48:02 +1000

    fix[py-build]: Avoid attempting to install fonts if parent directory isn't there

--- _internalUse/lib-building-python.inc.bash
+++ _internalUse/lib-building-python.inc.bash
@@ -158,12 +158,11 @@ function do_setupPythonVenv_orClean()
 
             font_link=".venv/lib/python3.${python3_subver}/site-packages/cv2/qt/fonts"
             font_target="/usr/share/fonts/truetype/dejavu"
-            if [[ ! -d "$font_link" ]] && [[ -d "$font_target" ]] ; then
+            if [[ ! -d "$font_link" ]] && [[ -d "$font_link/.." ]] && [[ -d "$font_target" ]] ; then
                 #
                 # Install fonts for QT apps - otherwise it complains about not finding them
                 #
                 do_ensure_link "$font_link" "$font_target"
-
             else
                 true
             fi
