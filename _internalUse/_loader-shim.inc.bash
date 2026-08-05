9a0d27df8c3d199cc9cd599444eab8d4536cfb0a
Mac Harwood <MacHarwood@gmail.com>
2026-08-05 14:49:13 +1000

    chore: Update readmes

--- _internalUse/_loader-shim.inc.bash
+++ _internalUse/_loader-shim.inc.bash
@@ -3,6 +3,8 @@
 #
 # SHIM Template for loading ukko_bashlibs in a way that is compatible with both direct sourcing and via git-shared-checkout
 #
+# Rev v0.0.3
+#
 # After sourcing this file, 'UKKO_BASHLIBS_LOCAL_DIR' & 'UKKO_BASHLIBS_DIR' are set
 # Typically:
 #    * UKKO_BASHLIBS_DIR is the actual directory of the ukko_bashlibs
@@ -37,6 +39,7 @@
 # │ # shellcheck source=/dev/null
 # │ source "$(dirname "$(realpath -m "${BASH_SOURCE[0]}")")/libs/shim-lib-building.inc.bash"
 # ╰─────────────────────────────────────────────────────────────────────────────────────
+export UKKO_SHIM_VERSION=v0.0.3
 
 UKKO_BASHLIBS_REF_PREFERRED=ver:v0.0.6
 
@@ -44,7 +47,6 @@ UKKO_BASHLIBS_REF_PREFERRED=ver:v0.0.6
 #
 #
 #
-
 function ukkoLibInstall()
 {
     export UKKO_BASHLIBS_LOCAL_DIR="${LIBS_DIR%/}/ukko_bashlibs"
@@ -78,6 +80,7 @@ function ukkoLibInstall()
             _ukko_lib_reason="git-shared-checkout (${download_refNote:-})"
         else
             echo "❌ Failed to find or download 'ukko_bashlibs' library.  Searched in parent directories and attempted to download with git-shared-checkout." >&2
+            echo "    Suggestion: Consider deleting 'git-shared-checkout' if this is causing issues"
             return 1
         fi
 
@@ -134,54 +137,20 @@ function _downloadItFromCloud()
 function ensure_installed_direct_if_needed()
 {
     local destRef="${1:-}"
+    local git_url="${2:-}"
+    local git_ref="${3:-}"
+
     local exe_name="${destRef##*/}"
+    # |ExtraLogging| echo "! $0 $@"
+    # |ExtraLogging| echo " - destRef=${destRef@Q}"
+    # |ExtraLogging| echo " - exe_name=${exe_name@Q}"
+    # |ExtraLogging| echo " - git_url=${git_url@Q}"
+    # |ExtraLogging| echo " - git_ref=${git_ref@Q}"
+
 
     command -v "$exe_name" &> /dev/null && return 0
 
-    local git_url="${2:-}"
-    local git_ref="${3:-}"
     local url_part=''
-
-
-    ##############################################
-    # Handle ver:
-    # (The first found of: [tag:###,branch:###,branch:###-wip])
-    if [[ "${git_ref}" == "ver:"* ]] ; then
-        local option_ref_value="${git_ref#ver:}"
-        # shellcheck disable=SC2016
-        readarray -t _revMarkers < <(cd /tmp && git ls-remote  "$git_url") || FATAL_FAILURE_NO_RETURN "Unable to access repository '$git_url'"
-
-        function _findRef()
-        {
-            local suffix="$1"
-            for ref in "${_revMarkers[@]}" ; do
-                [[ "$ref" == *"refs/${suffix}" ]] && return 0
-            done
-            return 1
-        }
-
-        if _findRef "tags/${option_ref_value}" ; then
-            option_ref_type='tag'
-            echo "ℹ️  Found ${option_ref_type}: ${option_ref_value}" >&2
-        elif _findRef "heads/${option_ref_value}" ; then
-            option_ref_type='branch'
-            echo "ℹ️  Found ${option_ref_type}: ${option_ref_value}" >&2
-        elif _findRef "heads/${option_ref_value}-wip" ; then
-            option_ref_type='branch'
-            option_ref_value="${option_ref_value}-wip"
-            echo "ℹ️  Found ${option_ref_type}: ${option_ref_value}" >&2
-        else
-            {
-                echo "ℹ️  Reviewed:"
-                for x in "${_revMarkers[@]}" ; do
-                    echo "  •  $x"
-                done
-            } >&2
-            FATAL_FAILURE_NO_RETURN "Neither tag:{$option_ref_value} nor branch:${option_ref_value} nor branch:${option_ref_value}-wip could be found in the repository '$git_url'"
-        fi
-
-        git_ref="${option_ref_type}:${option_ref_value}"
-    fi
 
     if [[ -z "${git_ref}" ]] ; then
         url_part="refs/heads/main"
@@ -191,8 +160,8 @@ function ensure_installed_direct_if_needed()
         url_part="refs/tags/${git_ref#tag:}"
     elif [[ "${git_ref}" == "hash:"* ]]; then
         url_part="${git_ref#hash:}"
-    else
-        FATAL_FAILURE_NO_RETURN "Invalid format for git_ref: ${git_ref@Q}.\nExpected formats:\n • branch:<branch_name>\n • tag:<tag_name>\n • hash:<hash_value>"
+   else
+        FATAL_FAILURE_NO_RETURN "Invalid format for git_ref: ${git_ref@Q}.\nExpected formats:\n • branch:<branch_name>\n • tag:<tag_name>\n • hash:<hash_value> • ver:<version>"
     fi
 
     local exe_url="${git_url#git@}"
@@ -267,13 +236,27 @@ ukkoLibInstall
 # Load directly if shim
 # if 'shim-lib-<libname>.inc.bash' is a link to this file then 'lib-<libname>.inc.bash' is automatically sourced
 _srcRef="${BASH_SOURCE[0]##*/}"
+_callerRef="${BASH_SOURCE[1]:-}"
+_callerRef="${_callerRef##*/}"
 # |ExtraLogging| echo "⚡  srcRef=$_srcRef" >&2
+
+_lib_fullname=''
 
 if [[ "$_srcRef" == 'shim-lib-'*'.inc.bash' ]] ; then
     _libname="${_srcRef#shim-lib-}"
-    _libname="${_libname%.inc.bash}"
+    _lib_fullname="lib-${_libname%.inc.bash}"
     # |ExtraLogging| echo "⚡  Detected shim for library '$_libname', loading it directly" >&2
     # |ExtraLogging| echo "⚡  Loading shim directly: ${UKKO_BASHLIBS_LOCAL_DIR%/}/lib-${_libname}.inc.bash" >&2
+elif [[ -n "${LIB_TO_SHIM:-}" ]] ; then
+    _lib_fullname="${LIB_TO_SHIM:-}"
+elif [[ "${_callerRef}" == *install* ]] || [[ "${_callerRef}" == *build* ]] ; then
+    _lib_fullname='lib-building'
+elif [[ "${_callerRef}" == *test* ]] ; then
+    _lib_fullname='lib-testing'
+fi
+if [[ -z "${_lib_fullname}" ]] ; then
+    echo "❌  ${_callerRef}: Expected 'LIB_TO_SHIM=lib-building' -or- lib-app -or- lib-common -or- lib-testing before 'source --/${BASH_SOURCE[0]##*/}'" >&2
+else
     # shellcheck source=/dev/null
-    source "${UKKO_BASHLIBS_LOCAL_DIR%/}/lib-${_libname}.inc.bash"
+    source "${UKKO_BASHLIBS_LOCAL_DIR%/}/${_lib_fullname}.inc.bash"
 fi
--- readme.md
+++ readme.md
@@ -1,12 +1,46 @@
-# `ukko_bashlibs` -- An old man's collection of bash utilities  (WIP: `0.0.6-wip`)
-
+# `ukko_bashlibs` -- An old man's collection of bash utilities  (WIP: `0.0.6-wip`) #
 
 ## Parts ##
 
 * **[do-run-in-docker ⧉](part_do-run-in-docker/readme.md)**
 * **[git-shared-checkout ⧉](part_git-shared-checkout/readme.md)**
-* **[Building and Installing your project ⧉](part_installer/readme.md)**
 
+## How to use in your project ##
+
+This is most commonly used to install a python project, such as:
+
+```tree
+├── do-build-and-install.sh
+├── my-app.py
+├── libs
+│   └── _loader-shim.inc.bash
+├── readme.md
+└── requirements.txt
+```
+
+Where:
+
+* **`libs/_loader-shim.inc.bash`** is: [**libs/_loader-shim.inc.bash**](_internalUse/_loader-shim.inc.bash) -and-
+* **`do-build-and-install.sh`** is:
+
+   ```bash
+   #!/bin/env bash
+   set -eu
+
+
+   function apps_doInstallOrClean()
+   {
+       echo "🔨 app-name"
+
+       installEditablePythonPkgs "git@github.com:smc-collaborate/ukko_pylibs"  --ref='ver:v0.2.0'
+
+       do_pyInstall "my-app.py"
+   }
+
+
+   # shellcheck source=/dev/null
+   source "$(dirname "$(realpath -m "${BASH_SOURCE[0]}")")/libs/_loader-shim.inc.bash"
+   ```
 
 ## Style ##
 
@@ -14,20 +48,10 @@ Style can be enforced with **`pre-commit install`**<br>
 
 Check with: **`pre-commit run -a`**
 
-
 ## Full Regression Testing ##
 
 This is done with `ukko_collections` - which has test scripts and includes `ukko_bashlibs` as a submodule
 
 ## Dev notes ##
 
-Will be deprecated:
-
-```
-git-shared-checkout  -> part_git-shared-checkout/git-shared-checkout
-do-run-in-docker     -> part_do-run-in-docker/do-run-in-docker
-utils.inc.bash       |  See note inside
-testSupport.inc.bash |
-test-funcs.inc.bash  |
-build-funcs.inc.bash |
-```
+None currently
