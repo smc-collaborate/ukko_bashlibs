eaf1ec9596f2ed693defbb668755724a16a1ee3d
Mac Harwood <MacHarwood@gmail.com>
2026-09-09 13:45:15 +1000

    fix: Ensure submodule status is done recursively

--- _internalUse/lib-git.inc.bash
+++ _internalUse/lib-git.inc.bash
@@ -237,8 +237,10 @@ function installFromGit()
 
 function git_failIfSubmodulesArentCloned()
 {
+
     # shellcheck disable=SC1091
-    if git -C "${1}" submodule 2>/dev/null | grep '^-' ; then
+    if git -C "${1}" submodule status --recursive 2>/dev/null | grep '^-' ; then
+        echo ""
         echo -e "⚠️  Submodules not loaded.  Please use: '${COLOUR[VIVID_BLUE_USED]:-}$(git_with_location_params_nice "${1}") submodule update --init --recursive${COLOUR[OFF_USED]:-}'"
         echo    "    (You could also have used 'git clone --recurse-submodules' when cloning originally)"
         exit 3
